ec162aa994f5292daba53de62f746cd1e9f67d3c
Mark Skelton <markskelton10@gmail.com>
2025-01-09 18:57:54 +0000

    Add Comparable module to Node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
-# Node class
+# Node class has methods for init, comparison, and to access left node, right node, and data
 class Node
+  include Comparable
+
   attr_accessor :left, :right, :data
 
   def initialize(data)
     @left = nil
     @right = nil
     @data = data
+  end
+
+  def <=>(other)
+    data <=> other.data
   end
 end
--- main.rb
+++ main.rb
@@ -19,3 +19,11 @@ node1.data = 100
 p node1.data
 p node1.left.data
 p node1.right.data
+
+puts 'Compare'
+p node2 < node3
+p node3 < node2
+p node1.between?(node2, node3)
+p node2.between?(node3, node1)
+sorted_array = [node1, node2, node3].sort
+sorted_array.each { |node| p node.data }
